4695dd05f172c14b0b7aa1271f35ff8db0374afe
hamza khan <maheenmalik97@gmail.com>
2022-12-12 12:00:12 +0000

    milestone 2 added to git file.

---
+++ milestone_2.py
@@ -0,0 +1,11 @@
+import random
+word_list = ['grapes', 'mango', 'pineapple', 'lychee', 'strawberry']
+fruit = random.choice(word_list)
+print(fruit)
+#input("Enter a single letter")
+guess = input("Enter a single letter\n")
+if len(guess) == 1 and guess.isalpha():
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input')
+print(guess)
